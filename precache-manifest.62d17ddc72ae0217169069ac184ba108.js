9d1fe752e514fd8c0f2c386377597cc1ec71276f
Jude Carpenter <judecarpenter@ymail.com>
2020-07-07 16:36:55 -0500

    Updates

---
+++ precache-manifest.62d17ddc72ae0217169069ac184ba108.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d0af993028b2db8449840f4a7c9550",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "4a0a65bb611b32954489",
+    "url": "/ReactPortfolio/static/css/main.7747be13.chunk.css"
+  },
+  {
+    "revision": "b302f1ff0a3ad8811a4b",
+    "url": "/ReactPortfolio/static/js/2.03a1c67f.chunk.js"
+  },
+  {
+    "revision": "9170794581eaac989d26146d9a215ccf",
+    "url": "/ReactPortfolio/static/js/2.03a1c67f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a0a65bb611b32954489",
+    "url": "/ReactPortfolio/static/js/main.c4ca2daf.chunk.js"
+  },
+  {
+    "revision": "2949d1041747ae56d829",
+    "url": "/ReactPortfolio/static/js/runtime-main.a65dbc00.js"
+  },
+  {
+    "revision": "b94ad3079d91249fd9ff5fbd906a31b6",
+    "url": "/ReactPortfolio/static/media/burger.b94ad307.jpg"
+  },
+  {
+    "revision": "17d54a698633911b81feb6f7d858a609",
+    "url": "/ReactPortfolio/static/media/clicker.17d54a69.jpg"
+  },
+  {
+    "revision": "0389b394497362cf58f6158c58e69725",
+    "url": "/ReactPortfolio/static/media/crystal.0389b394.jpg"
+  },
+  {
+    "revision": "b151c919b60585e32f1b6b9904cf7812",
+    "url": "/ReactPortfolio/static/media/destination.b151c919.jpg"
+  },
+  {
+    "revision": "1c57b3d18013a010675ab774dd56014d",
+    "url": "/ReactPortfolio/static/media/devhigher.1c57b3d1.jpg"
+  },
+  {
+    "revision": "65c2d7878e11568c6ef3bc9cf39c5bf3",
+    "url": "/ReactPortfolio/static/media/friends.65c2d787.jpg"
+  },
+  {
+    "revision": "631f62caaecc3b146874d8db904f8da4",
+    "url": "/ReactPortfolio/static/media/giphy.631f62ca.jpg"
+  },
+  {
+    "revision": "ef6e355b538a2aa0f5ddc0690d61ec54",
+    "url": "/ReactPortfolio/static/media/linkedinBackground.ef6e355b.png"
+  },
+  {
+    "revision": "767dd8f4784a43d3096d20cb28510ffc",
+    "url": "/ReactPortfolio/static/media/lyric.767dd8f4.png"
+  },
+  {
+    "revision": "7a04df97ad09e5fa7f8f1718e0be9fcb",
+    "url": "/ReactPortfolio/static/media/pic1.7a04df97.png"
+  },
+  {
+    "revision": "a78a18c0200c48b71322fa2d25ce7396",
+    "url": "/ReactPortfolio/static/media/restaurant.a78a18c0.jpg"
+  },
+  {
+    "revision": "318c157c0f8242bbfdac95e370194b10",
+    "url": "/ReactPortfolio/static/media/trivia.318c157c.jpg"
+  },
+  {
+    "revision": "3674d4cf70cb258ce72474c02db0a5d1",
+    "url": "/ReactPortfolio/static/media/word.3674d4cf.jpg"
+  }
+]);
